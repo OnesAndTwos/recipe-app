2b1fd31737c391ef1fc4fe37820fa417a7221d5a
mkay <mkay@thoughtworks.com>
2018-01-03 11:49:09 +0000

    Mike : Added

--- src/ai/search.js
+++ src/ai/search.js
@@ -2,4 +2,10 @@ const socket = require('../socket/server');
 
 module.exports = (parameters) => {
   socket.emit('recipe-event', { action: "SEARCH_RECIPE", searchTerm: parameters.searchTerm });
+
+  return {
+    "speech": "Searching for you now",
+    "displayText": "Searching for you now",
+    "source": "cookbook"
+  }
 };
